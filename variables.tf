ed4f668935a0da636c34aadd53982fbb33e3e477
Gabriel Garoz <gab.120gg@gmail.com>
2024-09-17 00:20:28 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,13 +23,13 @@ variable "cluster_name_suffix" {
   default     = ""
 }
 
-#variable "region" {
-#  description = "The region to host the cluster in"
-#}
-
-variable "zones" {
-  description = "The zones to host the cluster in"
+variable "region" {
+  description = "The region to host the cluster in"
 }
+
+#variable "zones" {
+#  description = "The zones to host the cluster in"
+#}
 
 variable "network" {
   description = "The VPC network to host the cluster in"
